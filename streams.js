71e6ffce88e6f583160b4c72660b25784ae5fa4f
shapikcool@gmail.com <shapikcool@gmail.com>
2017-11-27 15:54:14 +0200

    upd: csvToStings stream

--- streams.js
+++ streams.js
@@ -1,20 +1,36 @@
 const stream = require('stream');
+const fs = require('fs');
 
-class CsvToJSON extends stream.Transform {
-  constructor(options = {}){
+class CsvToStrings extends stream.Transform {
+  constructor(options = {}) {
+    options = Object.assign({}, options, {
+      decodeStrings: false
+    });
     super(options);
   }
 
   _transform(chunk, encoding, callback) {
-    if(encoding !== 'utf8') {
+    if (encoding !== 'utf8') {
       this.emit('error', new Error('Only UTF-8 sources are supported'));
       return callback();
     }
-
-    this.push();
+    chunk = chunk.split("\n");
+    let headers = chunk.shift().split(",");
+    let json = [];  
+      
+    chunk.forEach(function(d){
+        let tmp = {}
+        let row = d.split(",")
+        for(let i = 0; i < headers.length; i++){
+            tmp[headers[i]] = row[i];
+        }
+        json.push(tmp);
+    });
+    this.push(JSON.stringify(json));
     callback();
   }
 }
 
-
-
+fs.createReadStream('books.csv', 'utf8')
+  .pipe(new CsvToStrings())
+  .pipe(fs.createWriteStream('books.json'));
